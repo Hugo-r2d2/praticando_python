6d55d22393f8cdaccb80beb33892a0358cbeac31
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-01-30 11:07:12 -0400

    exercício - 1.2

---
+++ nv1_exc2.py
@@ -0,0 +1,12 @@
+# (2°) Declare três variáveis (idade, altura e peso) e mostre os valores.
+
+idade = 20
+altura = 1.75
+peso = 72
+
+print(f'''
+      Idade: {idade}
+      Altura: {altura}
+      Peso: {peso}kg
+      '''
+      )
