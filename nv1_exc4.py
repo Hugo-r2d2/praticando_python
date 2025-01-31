38efbf494e5ef2b57cd0ee1ab2a004f6e0ee5d89
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-01-31 18:12:34 -0400

    exercício - 1.4

---
+++ nv1_exc4.py
@@ -0,0 +1,6 @@
+# (4°) Peça para o usuário digitar dois números e mostre a soma deles.
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+
+print(f'A soma de {n1} + {n2} é = {n1 + n2}')
