ba13ba68951e71efa7367c913ac366d4b987d360
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-01 11:24:43 -0400

    exercício - 1.7

---
+++ nv1_exc7.py
@@ -0,0 +1,8 @@
+# (7°) Declare um dicionário com nome e idade de uma pessoa e exiba apenas a idade.
+
+cadastro = {
+    'nome' : 'Hugo Camelo dos Santos',
+    'idade' : 20,
+}
+
+print(cadastro['idade'])
