ab3e62b90fffe75cf64a5a8719736ef8bea445b6
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-03 17:44:36 -0400

    exercício - 1.21

---
+++ 3 - Estruturas Condicionais/nv1_exc_21.py
@@ -0,0 +1,8 @@
+# (21°) Peça um número ao usuário e exiba se ele é positivo ou negativo.
+
+n = int(input("Digite um número: "))
+
+if n < 0:
+    print("Número negativo")
+else: 
+    print("Número positivo")
