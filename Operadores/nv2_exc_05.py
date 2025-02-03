6636e52d8fe623ff4eb87c8453b58961aa8fa7cc
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-03 16:03:44 -0400

    exercício - 2.05

---
+++ Operadores/nv2_exc_05.py
@@ -0,0 +1,9 @@
+# (5°) Calcule o quadrado e o cubo de um número digitado pelo usuário.
+
+n1 = int(input("Digite um número: "))
+
+quad = n1**2
+cub = n1**3
+
+print(f"O quadrado de {n1} é: {quad}")
+print(f"O cubo de {n1} é: {cub}")
