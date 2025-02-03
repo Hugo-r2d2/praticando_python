621888133ce5b110377fbc62b8cc4ccdf47b27f6
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-03 17:05:05 -0400

    exercício - 1.18

---
+++ Operadores/nv1_exc_18.py
@@ -0,0 +1,21 @@
+# (18°) Use operadores lógicos (and, or, not) para avaliar três expressões verdadeiras e falsas.
+
+# Usando operador AND
+if (1 + 1 and 2 + 0) == 2:
+    print("Verdadeiro")
+else:
+    print("Falso")
+
+# Usando operador OR
+if (1 + 1 or 2 + 1) == 2:
+    print("Verdadeiro")
+else:
+    print("Falso")
+
+# Usando operador NOT
+if not 1 + 1 == 2:
+    print("Verdadeiro")
+else: 
+    print("Falso")
+
+
