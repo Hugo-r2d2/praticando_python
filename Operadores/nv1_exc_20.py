18993a6f0e3944789ca6ca6c8e0a11d0410f3145
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-03 17:16:15 -0400

    exercício - 1.20

---
+++ Operadores/nv1_exc_20.py
@@ -0,0 +1,8 @@
+# (20°) Peça um número ao usuário e verifique se ele está entre 10 e 100.
+
+n1 = int(input("Digite um número: "))
+
+if n1 >= 10 and n1 <= 100:
+    print("O número está entre 10 e 100")
+else: 
+    print("O número não está entre 10 e 100")
