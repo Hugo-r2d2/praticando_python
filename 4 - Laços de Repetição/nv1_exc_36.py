87ccb3327c9ea3ba9f96ca8a6942ca1e1803d51e
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-04 16:38:56 -0400

    exercício - 1.36

---
+++ 4 - Laços de Repetição/nv1_exc_36.py
@@ -0,0 +1,11 @@
+# (36°) Crie um programa que peça 5 números e exiba a média.
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+n3 = int(input("Digite um número: "))
+n4 = int(input("Digite um número: "))
+n5 = int(input("Digite um número: "))
+
+media = (n1 + n2 + n3 + n4 + n5) / 5
+
+print(f"A média é: {media}")
