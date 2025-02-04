79503b8cff55e5b19b79c79900171b42b92e5394
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-04 15:30:25 -0400

    exercício - 1.32

---
+++ 4 - Laços de Repetição/nv1_exc_32.py
@@ -0,0 +1,10 @@
+# (32°) Some os números de 1 a 100 e exiba o resultado.
+
+contador = 1
+soma = 0
+
+while contador <= 100:
+    soma += contador
+    contador += 1
+
+print(f'A soma dos valor de 1 a 100 é: {soma}')
