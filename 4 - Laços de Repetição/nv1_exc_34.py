ad647881d6803edbe32cf1947d62e4fa57b0f93e
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-04 16:14:09 -0400

    exercício - 1.34

---
+++ 4 - Laços de Repetição/nv1_exc_34.py
@@ -0,0 +1,7 @@
+# (34°) Conte de 10 até 0 e exiba "Fogo!" ao final.
+
+for i in range(10, -1, -1):  # Começa em 10, vai até 0, decrementando de 1 em 1
+    print(i)
+
+print("Fogo!")  # Exibe a mensagem ao final da contagem
+
