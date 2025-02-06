97a6045d80c6f6a0e17405d895b4c6a60830aac9
Hugo Camelo dos Santos <ihugocs@gmail.com>
2025-02-05 23:14:03 -0400

    exercício - 1.42

---
+++ 5 - Funções/nv1_exc_42.py
@@ -0,0 +1,7 @@
+# (42°) Faça uma função que retorna o quadrado de um número.
+
+def quad_numero(n):
+    print(f"O quadrado de {n} é: {n**2}")
+
+n = int(input("Digite um número: "))
+quad_numero(n)
